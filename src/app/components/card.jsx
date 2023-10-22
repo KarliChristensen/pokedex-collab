98b0c97bdcadfc8033006f0c783d5afcf3f34811
Simon Björkberg <127056123+SimonBjorkberg@users.noreply.github.com>
2023-10-22 03:43:32 +0200

    image changes to shiny version

--- src/app/components/card.jsx
+++ src/app/components/card.jsx
@@ -2,28 +2,47 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 export default function PokemonCard({ pokemon, getOnePokemon }) {
+  const [hoverStatus, setHoverStatus] = useState(false);
+  const [hoverImage, setHoverImage] = useState(false);
+  const [noHoverImage, setNoHoverImage] = useState("");
+  const [image, setImage] = useState("");
   const [pokemonInfo, setPokemonInfo] = useState({});
 
   const getPokemonInfo = async () => {
     const response = await axios.get(pokemon.url);
     setPokemonInfo(response.data);
+    setNoHoverImage(response.data.sprites.front_default);
+    setHoverImage(response.data.sprites.front_shiny);
+    setImage(response.data.sprites.front_default);
+  };
+
+  const changePicture = () => {
+    if (!hoverStatus) {
+      setImage(noHoverImage);
+    }
+    if (hoverStatus) {
+      setImage(hoverImage);
+    }
   };
 
   useEffect(() => {
     getPokemonInfo();
+    setImage(pokemonInfo?.sprites?.front_default);
   }, []);
 
-  console.log(pokemonInfo);
+  useEffect(() => {
+    changePicture();
+  }, [hoverStatus]);
 
   return (
     <div
-      className="border border-neutral h-fit hover:cursor-pointer hover:opacity-60"
+      className="border border-neutral h-fit hover:cursor-pointer"
       onClick={() => getOnePokemon(pokemon?.name)}
+      onMouseEnter={() => setHoverStatus(true)}
+      onMouseLeave={() => setHoverStatus(false)}
     >
-      <img src={pokemonInfo.sprites?.front_default} alt="sprite" />
-      <p className="text-center">
-        {pokemonInfo.species?.name}
-      </p>
+      <img src={image} alt="sprite" />
+      <p className="text-center">{pokemonInfo.species?.name}</p>
     </div>
   );
 }
